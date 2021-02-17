406a5a659064c72cfb05d507343a82d3c5cfe480
Ales Kubicek <ales.kubicek@student.manchester.ac.uk>
2021-02-17 22:01:21 +0100

    Add complex tensor representation

---
+++ src/TornadoQSim/src/main/java/uk/ac/manchester/tornado/qsim/math/ComplexTensor.java
@@ -0,0 +1,202 @@
+package uk.ac.manchester.tornado.qsim.math;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Represents a complex tensor with arbitrary shape (number of dimensions).
+ * @author Ales Kubicek
+ */
+public class ComplexTensor {
+    private final int rank;
+    private final int size;
+    private final int[] shape;
+
+    private final double[] real;
+    private final double[] imag;
+
+    /**
+     * Constructs an empty complex tensor of required shape.
+     * @param shape shape (dimensions) of the created tensor.
+     */
+    public ComplexTensor(int... shape) {
+        if (shape == null || shape.length < 1 || !allPositive(shape))
+            throw new IllegalArgumentException("Invalid tensor shape provided.");
+
+        this.rank = shape.length == 1 && shape[0] == 1 ? 0 : shape.length;
+        this.size = calculateSize(shape);
+        this.shape = shape;
+
+        this.real = new double[this.size];
+        this.imag = new double[this.size];
+    }
+
+    /**
+     * Constructs a complex tensor of required shape filled with supplied complex data.
+     * @param data complex data.
+     * @param shape shape (dimensions) of the created tensor.
+     */
+    public ComplexTensor(Complex[] data, int... shape) {
+        this(shape);
+
+        if (data == null || data.length != this.size)
+            throw new IllegalArgumentException("Invalid tensor data provided.");
+
+        int i = 0;
+        for (Complex element : data) {
+            this.real[i] = element.real();
+            this.imag[i] = element.imag();
+            i++;
+        }
+    }
+
+    /**
+     * Constructs a complex tensor as a clone of a supplied complex tensor.
+     * @param original complex tensor to be cloned.
+     */
+    public ComplexTensor(ComplexTensor original) {
+        if (original == null)
+            throw new IllegalArgumentException("Invalid original tensor provided.");
+
+        this.rank = original.rank;
+        this.size = original.size;
+        this.shape = Arrays.copyOf(original.shape, original.shape.length);
+        this.real = Arrays.copyOf(original.real, original.real.length);
+        this.imag = Arrays.copyOf(original.imag, original.imag.length);
+    }
+
+    /**
+     * Gets a rank of this complex tensor (eg. rank 1 = vector, rank 2 = matrix, ...).
+     * @return complex tensor rank.
+     */
+    public int rank() { return this.rank; }
+
+    /**
+     * Gets a total size of this complex tensor (total number of complex data points).
+     * @return total number of complex data points.
+     */
+    public int size() { return this.size; }
+
+    /**
+     * Gets a shape (dimensions) of this complex tensor.
+     * @return complex tensor shape.
+     */
+    public int[] shape() { return this.shape; }
+
+    /**
+     * Gets all real parts of this complex tensor.
+     * @return all real parts.
+     */
+    public double[] getRawRealData() { return this.real; }
+
+    /**
+     * Gets all imaginary parts of this complex tensor.
+     * @return all imaginary parts.
+     */
+    public double[] getRawImagData() { return this.imag; }
+
+    /**
+     * Retrieves a single indexed complex element from this complex tensor.
+     * @param indicies location within the complex tensor.
+     * @return single complex number (at the supplied index).
+     */
+    public Complex getElement(int... indicies) {
+        checkIndexBounds(indicies);
+        int i = getFlatIndex(indicies);
+        return new Complex(real[i], imag[i]);
+    }
+
+    /**
+     * Inserts the supplied complex element into the complex tensor at specified index.
+     * @param element complex number to be inserted.
+     * @param indicies location within the complex tensor.
+     */
+    public void insertElement(Complex element, int... indicies) {
+        if (element == null)
+            throw new IllegalArgumentException("Invalid tensor element provided.");
+        checkIndexBounds(indicies);
+        int i = getFlatIndex(indicies);
+        real[i] = element.real();
+        imag[i] = element.imag();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ComplexTensor that = (ComplexTensor) o;
+        return rank == that.rank && size == that.size && Arrays.equals(shape, that.shape)
+                && Arrays.equals(real, that.real) && Arrays.equals(imag, that.imag);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(rank, size);
+        result = 31 * result + Arrays.hashCode(shape);
+        result = 31 * result + Arrays.hashCode(real);
+        result = 31 * result + Arrays.hashCode(imag);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder finalString = new StringBuilder();
+        finalString.append("ComplexTensor {"
+                + " rank: " + this.rank
+                + ", shape: " + Arrays.toString(shape)
+                + ", data: [");
+        for (int i = 0; i < this.size; i++)
+            if (i == this.size - 1)
+                finalString.append(new Complex(this.real[i], this.imag[i]) + "] }");
+            else
+                finalString.append(new Complex(this.real[i], this.imag[i]) + ", ");
+        return finalString.toString();
+    }
+
+    private void checkIndexBounds(int... indicies) {
+        if (!allPositiveOrZero(indicies))
+            throw new IndexOutOfBoundsException("Index cannot be negative.");
+        if (indicies.length != this.shape.length)
+            throw new IndexOutOfBoundsException("Number of supplied indicies does not correspond to the tensor rank.");
+        for (int i = 0; i < indicies.length; i++)
+            if (indicies[i] >= this.shape[i])
+                throw  new IndexOutOfBoundsException("Supplied index does not fit the tensor shape.");
+    }
+
+    private int getFlatIndex(int... indicies) {
+        if (indicies == null)
+            return 0;
+        // a * D1*D2*D3*Dn + b * D1*D2*D3*Dn-1 + ... + y * D1 + z
+        // where: (a,b,...,y,z)  ... indicies of the element to be accessed
+        //        D1,D2,D3,Dn    ... dimensions of the tensor
+        int flatIndex = 0;
+        for (int i = 0; i < indicies.length; i++) {
+            int dimensionFactor = 1;
+            for (int j = 0; j < (indicies.length - i - 1); j++)
+                dimensionFactor *= this.shape[j];
+            flatIndex += indicies[i] * dimensionFactor;
+        }
+        return flatIndex;
+    }
+
+    private int calculateSize(int[] shape) {
+        int dataSize = 1;
+        for (int value : shape)
+            dataSize *= value;
+        return dataSize;
+    }
+
+    private boolean allPositive(int[] numbers) {
+        for (int number : numbers)
+            if (number <= 0)
+                return false;
+        return true;
+    }
+
+    private boolean allPositiveOrZero(int[] numbers) {
+        for (int number : numbers)
+            if (number < 0)
+                return false;
+        return true;
+    }
+}
