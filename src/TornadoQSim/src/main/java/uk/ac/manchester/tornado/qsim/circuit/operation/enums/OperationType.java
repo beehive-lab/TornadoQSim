1ff3e1cf6efb5f19467f9a7f60fd8c066cf23dea
Ales Kubicek <ales.kubicek@student.manchester.ac.uk>
2021-02-28 13:29:45 +0100

    Add javadoc description for operation types

--- src/TornadoQSim/src/main/java/uk/ac/manchester/tornado/qsim/circuit/operation/enums/OperationType.java
+++ src/TornadoQSim/src/main/java/uk/ac/manchester/tornado/qsim/circuit/operation/enums/OperationType.java
@@ -1,5 +1,9 @@
 package uk.ac.manchester.tornado.qsim.circuit.operation.enums;
 
+/**
+ * Enumerates possible types of quantum operations.
+ * @see uk.ac.manchester.tornado.qsim.circuit.operation.Operation for more details.
+ */
 public enum OperationType {
     Gate,
     ControlGate,
