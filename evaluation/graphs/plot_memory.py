52c27093913cac8852eb757b28b512c3c449987b
Ales Kubicek <ales.kubicek@student.manchester.ac.uk>
2021-03-22 12:17:34 +0100

    Add experiment data points

--- evaluation/graphs/plot_memory.py
+++ evaluation/graphs/plot_memory.py
@@ -2,39 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import data
 
-qubits = np.arange(1, 30)
-data = data.get_experiment_data("entanglement")
+qubits = np.arange(1, 16)
+data = data.get_experiment_data("qft")
 
-x = data[0][:, 0]
-y = data[0][:, 4]
+if data[0].size > 0:
+    x = data[0][:, 0]
+    y = data[0][:, 4]
+    plt.plot(x, y, label="Qiskit", marker="o", linestyle=":", linewidth=1.0, color="#848484")
 
-plt.plot(x, y, label="Qiskit", marker="o", linestyle=":", linewidth=1.0, color="#848484")
+if data[1].size > 0:
+    x = data[1][:, 0]
+    y = data[1][:, 4]
+    plt.plot(x, y, label="Strange", marker="o", linestyle=":", linewidth=1.0, color="#545454")
 
+if data[2].size > 0:
+    x = data[2][:, 0]
+    y = data[2][:, 4]
+    plt.plot(x, y, label="Cirq", marker="o", linestyle=":", linewidth=1.0, color="#262626")
 
-x = data[1][:, 0]
-y = data[1][:, 4]
+if data[3].size > 0:
+    x = data[3][:, 0]
+    y = data[3][:, 4]
+    plt.plot(x, y, label="TornadoQSim (non-accelerated)", marker="o", linestyle=":", linewidth=1.0, color="#7cacf6")
 
-plt.plot(x, y, label="Strange", marker="o", linestyle=":", linewidth=1.0, color="#545454")
+if data[4].size > 0:
+    x = data[4][:, 0]
+    y = data[4][:, 4]
+    plt.plot(x, y, label="TornadoQSim (Intel Core i7)", marker="o", linestyle=":", linewidth=1.0, color="#3b7ee6")
 
-x = data[2][:, 0]
-y = data[2][:, 4]
-
-plt.plot(x, y, label="Cirq", marker="o", linestyle=":", linewidth=1.0, color="#262626")
-
-x = data[3][:, 0]
-y = data[3][:, 4]
-
-plt.plot(x, y, label="TornadoQSim (non-accelerated)", marker="o", linestyle=":", linewidth=1.0, color="#7cacf6")
-
-x = data[4][:, 0]
-y = data[4][:, 4]
-
-plt.plot(x, y, label="TornadoQSim (Intel Core i7)", marker="o", linestyle=":", linewidth=1.0, color="#3b7ee6")
-
-x = data[5][:, 0]
-y = data[5][:, 4]
-
-plt.plot(x, y, label="TornadoQSim (Nvidia Quadro GP100)", marker="o", linestyle=":", linewidth=1.0, color="#002d8c")
+if data[5].size > 0:
+    x = data[5][:, 0]
+    y = data[5][:, 4]
+    plt.plot(x, y, label="TornadoQSim (Nvidia Quadro GP100)", marker="o", linestyle=":", linewidth=1.0, color="#002d8c")
 
 # Define graph formalities
 plt.legend(loc="lower right")
--- evaluation/graphs/plot_time.py
+++ evaluation/graphs/plot_time.py
@@ -2,50 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import data
 
-qubits = np.arange(1, 30)
-data = data.get_experiment_data("entanglement")
+qubits = np.arange(1, 16)
+data = data.get_experiment_data("qft")
 
-x = data[0][:, 0]
-y = data[0][:, 2]
-e_max = data[0][:, 1] - y
-e_min = y - data[0][:, 3]
+if data[0].size > 0:
+    x = data[0][:, 0]
+    y = data[0][:, 2]
+    e_max = data[0][:, 1] - y
+    e_min = y - data[0][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="Qiskit", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#848484")
 
-plt.errorbar(x, y, yerr=[e_min, e_max], label="Qiskit", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#848484")
+if data[1].size > 0:
+    x = data[1][:, 0]
+    y = data[1][:, 2]
+    e_max = data[1][:, 1] - y
+    e_min = y - data[1][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="Strange", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#545454")
 
-x = data[1][:, 0]
-y = data[1][:, 2]
-e_max = data[1][:, 1] - y
-e_min = y - data[1][:, 3]
+if data[2].size > 0:
+    x = data[2][:, 0]
+    y = data[2][:, 2]
+    e_max = data[2][:, 1] - y
+    e_min = y - data[2][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="Cirq", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#262626")
 
-plt.errorbar(x, y, yerr=[e_min, e_max], label="Strange", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#545454")
+if data[3].size > 0:
+    x = data[3][:, 0]
+    y = data[3][:, 2]
+    e_max = data[3][:, 1] - y
+    e_min = y - data[3][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (non-accelerated)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#7cacf6")
 
-x = data[2][:, 0]
-y = data[2][:, 2]
-e_max = data[2][:, 1] - y
-e_min = y - data[2][:, 3]
+if data[4].size > 0:
+    x = data[4][:, 0]
+    y = data[4][:, 2]
+    e_max = data[4][:, 1] - y
+    e_min = y - data[4][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (Intel Core i7)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#3b7ee6")
 
-plt.errorbar(x, y, yerr=[e_min, e_max], label="Cirq", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#262626")
-
-x = data[3][:, 0]
-y = data[3][:, 2]
-e_max = data[3][:, 1] - y
-e_min = y - data[3][:, 3]
-
-plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (non-accelerated)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#7cacf6")
-
-x = data[4][:, 0]
-y = data[4][:, 2]
-e_max = data[4][:, 1] - y
-e_min = y - data[4][:, 3]
-
-plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (Intel Core i7)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#3b7ee6")
-
-x = data[5][:, 0]
-y = data[5][:, 2]
-e_max = data[5][:, 1] - y
-e_min = y - data[5][:, 3]
-
-plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (Nvidia Quadro GP100)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#002d8c")
+if data[5].size > 0:
+    x = data[5][:, 0]
+    y = data[5][:, 2]
+    e_max = data[5][:, 1] - y
+    e_min = y - data[5][:, 3]
+    plt.errorbar(x, y, yerr=[e_min, e_max], label="TornadoQSim (Nvidia Quadro GP100)", marker="o", linestyle=":", linewidth=1.0, capsize=2, color="#002d8c")
 
 # Define graph formalities
 plt.legend(loc="lower right")
